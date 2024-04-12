f572383785732becd9ec65b590886948752a4ed3
Sagar <sagarpuniyani@gmail.com>
2024-04-12 18:44:48 +0530

    Update login page, add signup page, and fix bugs in page.tsx

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LogIn = () => {
+return (
+    <h3>
+        LogIn page 
+    </h3>
+)
+}
+
+export default LogIn
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignUp = () => {
+return (
+    <h3>
+        SignUp page 
+    </h3>
+)
+}
+
+export default SignUp
