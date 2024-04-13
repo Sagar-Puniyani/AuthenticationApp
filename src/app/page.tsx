3f25e0f3b3c58ec8ed2bd868fbe89e6d634172db
Sagar <sagarpuniyani@gmail.com>
2024-04-13 10:13:29 +0530

    Update login page, add signup page, and fix bugs in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
 import LogIn from "./login/page";
 import SignUp from "./signup/page";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02] " >
-    < SignUp />
+    <main className="flex flex-col items-center justify-center min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02] " >
+      <Link href='/signup' >
+        <button
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">
+          Sign Up
+        </button>
+      </Link>
+      <br />
+
+      <Link href='/login' >
+        <button
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600">
+          Log In
+        </button>
+      </Link>
     </main>
   );
 }
